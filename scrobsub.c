e70214ab31ad278adc6e5c83b45c1527b301f7e0
Max Howell <max@methylblue.com>
2009-05-04 17:33:25 +0100

    Percent encode required parameters

--- scrobsub.c
+++ scrobsub.c
@@ -108,6 +108,37 @@ static bool ok(char* response)
     return response[0] == 'O' && response[1] == 'K' && response[2] == '\n';
 }
 
+static char* escape(const char* in)
+{
+    // code from QByteArray.cpp, Qt 4.4.0
+    
+    const char hexnumbers[] = "0123456789ABCDEF"; 
+    #define toHexHelper(c) hexnumbers[(c) & 0xf]
+    
+    int const n = strlen(in);
+    char outs[n*3];
+    char* out = outs;
+    for(int i = 0; i < n; ++i){
+        char c = *in++;
+        if(c >= 0x61 && c <= 0x7A // ALPHA
+           || c >= 0x41 && c <= 0x5A // ALPHA
+           || c >= 0x30 && c <= 0x39 // DIGIT
+           || c == 0x2D // -
+           || c == 0x2E // .
+           || c == 0x5F // _
+           || c == 0x7E)// ~
+            *out++ = c;
+        else{
+            *out++ = '%';
+            *out++ = toHexHelper((c & 0xf0) >> 4);
+            *out++ = toHexHelper(c & 0xf);
+        }
+    }
+    *out = '\0';
+
+    return strdup(outs);
+}
+
 static void handshake()
 {
     scrobsub_finish_auth();
@@ -115,10 +146,11 @@ static void handshake()
     if (!scrobsub_session_key || !scrobsub_username)
         return; //TODO auth required
     
+    char* username = escape(scrobsub_username);
     time_t time = now();
     char auth[33];
     get_handshake_auth(auth, time);
-    int n = 34+8+8+6+11+3+strlen(scrobsub_username)+13+32+9+32+4+32+1;
+    int n = 34+8+8+6+11+3+strlen(username)+13+32+9+32+4+32+1;
     char url[n];
     
     n = snprintf(url, n, "http://post.audioscrobbler.com:80/"
@@ -131,13 +163,15 @@ static void handshake()
                  "&a=%s" // length 32
                  "&api_key=" SCROBSUB_API_KEY // length 32
                  "&sk=%s", // length 32
-                 scrobsub_username, time, auth, scrobsub_session_key);
+                 username, time, auth, scrobsub_session_key);
+    free(username);
+
     if (n<0) return; //TODO error callback
 
     char responses[256];
-    char* response = responses;
     scrobsub_get(responses, url);
-
+    
+    char* response = responses;
     if(ok(response)){
         response += 3;
         session_id = handshake_response_strdup(&response);
@@ -162,7 +196,7 @@ static void submit()
     if(state == SCROBSUB_PAUSED)
         scrobsub_resume(); //sets pause time correctly
     //FIXME the second resolution issue can introduce rounding errors
-    if(now() - (start_time + pause_time) < scrobble_time(duration))
+    if(now()-(start_time+pause_time) < scrobble_time(duration))
         return;
     
     int n = 32+N+4+2+10+1+1 +2+9*6;
@@ -192,18 +226,16 @@ static void submit()
     }
 }
 
-
 void scrobsub_change_metadata(const char* _artist, const char* _track, const char* _album)
 {
     free(artist);
     free(track);
     free(album);
-    artist = strdup(_artist);
-    track = strdup(_track);
-    album = strdup(_album);    
+    artist = escape(_artist);
+    track = escape(_track);
+    album = escape(_album);    
     N = strlen(artist)+strlen(track)+strlen(album)+strlen(mbid);
 }
-
 
 void scrobsub_start(const char* _artist, const char* _track, unsigned int _duration, const char* _album, unsigned int _track_number, const char* _mbid)
 {
