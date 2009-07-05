00fd88cf0d06a9f3a3fdd05ae3df95baa1585867
Max Howell <max@methylblue.com>
2009-07-05 15:16:34 +0100

    Fix for non MS compilers

--- scrobsub.h
+++ scrobsub.h
@@ -25,12 +25,12 @@
 #ifndef __SCROBSUB_H__
 #define __SCROBSUB_H__
 
-#if _MSC_VER <= 1400
+#if defined(_MSC_VER) && _MSC_VER <= 1400
  #define SCROBSUB_NO_C99 1
 #endif
 
 #if SCROBSUB_NO_C99
- #if !__cplusplus
+ #if !__cplusplus && !defined(bool)
   #define bool int
   #define false 0
   #define true 1
@@ -44,53 +44,53 @@
 extern "C" {
 #endif
 
-/** the callback must be set, when the callback is called, you get one of the
-  * SCROBSUB_ values. */
+/** Indeed, call this at your earliest convenience! */
 void scrobsub_init(void(*callback)(int event, const char* message));
 
-/** you need to call scrobsub_auth, but you can do it whenever you want,
-  * although, no scrobbling will happen until then */
+/** @see scrobsub_auth. Scrobbling will not function until your application is
+  * auth'd. However we will be caching scrobbles in memory. */
 #define SCROBSUB_AUTH_REQUIRED 0
-/** the char* paramater will be the error string */
+/** The char* paramater will be the error string */
 #define SCROBSUB_ERROR_RESPONSE 1
 
 
-/** the user needs to visit @p url within one hour for authentication to succeed */
+/** The URL at url needs to be visited by the user in a web browser withing an
+  * hour to authorize your application for scrobbling. @see
+  * SCROBSUB_AUTH_REQUIRED. */
 void scrobsub_auth(char url[110]);
-
-/** once the user has visited the above page, we need to request their session
+/** Once the user has visited the above page, we need to request their session
   * key, you can either do this yourself, or scrobsub will do it as required,
   * mostly you won't need to call this function
   * @returns true if authentication was successful */
 bool scrobsub_finish_auth();
 
 
-/** A new track started. scrobsub takes copies of the strings. All strings must
-  * be UTF8. 
-  * artist, track and duration are mandatory
-  * album and mbid can be "" (do not pass NULL or 0)
-  * if track_number is 0 it is ignored (yes, we know some albums have a zeroth track)
-  * valid MBIDs are always 38 characters, be wary of that
-  */
+/** A new track started.
+  * We dup' the strings, so don't worry yourself with memory woes.
+  * All strings must be UTF8.
+  * artist, track and duration are mandatory.
+  * album and mbid can be "" (do *not* pass NULL or 0).
+  * A value of 0 for track_number will be ignored.
+  * You should note that valid MBIDs are always 38 characters. */
 void scrobsub_start(const char* artist, const char* track, unsigned int duration, const char* album, unsigned int track_number, const char* mbid);
-/** the thing that we're scrobbling got paused. This is not a toggle, when/if
+/** The track we are scrobbling became paused. This is NOT a toggle! When/if
   * the track is unpaused, call resume. We insist on this distinction because
   * we want you to be exact! */
 void scrobsub_pause();
-/** the thing that we're scrobbling was unpaused */
+/** The thing that we're scrobbling was unpaused */
 void scrobsub_resume();
-/** only call this when playback stops, if a new track is about to start, call
+/** Only call this when playback stops, if a new track is about to start, call
   * scrobsub_start() instead */
 void scrobsub_stop();
 
-    
-/** marks the current track as loved, it is worth noting, you also have to call
-  * the Last.fm track.love webservice separately (scrobsub doesn't do it for 
-  * you). This stupid system will be like this forever prolly. Sorry about that.
+
+/** Marks the current track as loved, it is worth noting, you also have to call
+  * the Last.fm track.love webservice separately (scrobsub doesn't do it for
+  * you). This should be fixed with Audioscrobbler protocol 2.0
   */
 void scrobsub_love();
 
-/** support this if you can and it is appropriate */
+/** Support this if you can and it is appropriate */
 void scrobsub_change_metadata(const char* artist, const char* track, const char* album);
 
 
@@ -101,21 +101,22 @@ void scrobsub_change_metadata(const char* artist, const char* track, const char*
 int scrobsub_state();
 
 
-/** returns 0 if you need to auth, or the user still hasn't allowed the auth 
-  * attempt */
+/** If these are NULL then authorization has not yet been completed */
 extern char* scrobsub_session_key;
 extern char* scrobsub_username;
 
 
-/** for your convenience, we need it, so maybe you can use it too, we use the
+/** For your convenience, we need it, so maybe you can use it too. We use the
   * platform native library to generate this md5 */
 void scrobsub_md5(char out[33], const char* in);
 
 
-#define SCROBSUB_VERSION 0x00020000
-#define SCROBSUB_MAJOR_VERSION 2
-#define SCROBSUB_MINOR_VERSION 0
-#define SCROBSUB_PATCH_VERSION 0
+/** This will always be the same as the protocol version we implement */
+#define SCROBSUB_VERSION 0x00010201
+#define SCROBSUB_VERSION_STRING "1.2.1"
+#define SCROBSUB_MAJOR_VERSION 1
+#define SCROBSUB_MINOR_VERSION 2
+#define SCROBSUB_PATCH_VERSION 1
 
 #if __cplusplus
 }
